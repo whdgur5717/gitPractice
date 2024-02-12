3f6ee43eb9d0e84a4b2d178bd41875657a50d810
이종혁 <whdgur57117@gmail.com>
2024-02-12 20:52:05 +0900

    로그 찍어보기

--- .github/actions/core/action.js
+++ .github/actions/core/action.js
@@ -4,7 +4,7 @@ import { context, getOctokit } from "@actions/github";
 try {
 	// `who-to-greet` input defined in action metadata file
 	const reviewers = getInput("reviewers");
-	console.log(context.repo);
+	console.log(context.repo, context.payload);
 	const token = getInput("github_token");
 } catch (error) {
 	setFailed(error.message);
