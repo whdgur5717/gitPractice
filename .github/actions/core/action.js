ec6c527212b48fa0f6f0d531bd37d975a766555d
이종혁 <whdgur57117@gmail.com>
2024-02-12 21:07:46 +0900

    로직 수정

--- .github/actions/core/action.js
+++ .github/actions/core/action.js
@@ -4,7 +4,7 @@ import { context, getOctokit } from "@actions/github";
 try {
 	const reviewers = getInput("reviewers");
 	const token = getInput("github_token");
-	const octokit = github.getOctokit(token);
+	const octokit = getOctokit(token);
 
 	const { owner, repo } = github.context.repo;
 	const prNumber = github.context.payload.pull_request.number;
