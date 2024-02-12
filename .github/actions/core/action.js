9517ef6a6f36b32f0019f8c3997a31ee8c98fb84
이종혁 <whdgur57117@gmail.com>
2024-02-12 20:50:54 +0900

    repo 로그찍어보기

--- .github/actions/core/action.js
+++ .github/actions/core/action.js
@@ -3,16 +3,9 @@ import { context, getOctokit } from "@actions/github";
 
 try {
 	// `who-to-greet` input defined in action metadata file
-	const nameToGreet = getInput("reviewers");
+	const reviewers = getInput("reviewers");
+	console.log(context.repo);
 	const token = getInput("github_token");
-	const octokit = getOctokit(token);
-	console.log(octokit);
-	console.log(`Hello ${nameToGreet}!`);
-	const time = new Date().toTimeString();
-	setOutput("time", time);
-	// Get the JSON webhook payload for the event that triggered the workflow
-	const payload = JSON.stringify(context.payload, undefined, 2);
-	console.log(`The event payload: ${payload}`);
 } catch (error) {
 	setFailed(error.message);
 }
